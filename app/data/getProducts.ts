43aac43d21f2d861c64c508d18567eb177bb281a
rhygon <rhygonfn@gmail.com>
2025-06-23 07:52:37 -0400

    fixed cust price

--- app/data/getProducts.ts
+++ app/data/getProducts.ts
@@ -31,14 +31,465 @@ export default async function getProducts() {
         dispatch: "soon",
         inStock: true,
         media: [
-          "https://6jax627y1avwmget.public.blob.vercel-storage.com/france-in-pictures-beautiful-places-to-photograph-eiffel-tower-pn3n5wYyPqnueaNLkNyQyccgSZ9YO2.jpg",
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
         ],
         description: "DISC",
         price: 20,
         customPrice: 30,
         unstitchPrice: 40,
         createdAt: JSON.stringify(Date.now())
-      }
+      },
+      {
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },{
+        id: uuidv4(),
+        title: "TITLE",
+        type: "Readymade suits",
+        sizes: ["XS", "S"],
+        dispatch: "soon",
+        inStock: true,
+        media: [
+          "https://6jax627y1avwmget.public.blob.vercel-storage.com/1037518551-5PrNDsqMtyRqmlUVaPfp33MmI1OZsR.jpg",
+        ],
+        description: "DISC",
+        price: 20,
+        customPrice: 30,
+        unstitchPrice: 40,
+        createdAt: JSON.stringify(Date.now())
+      },
     ],
     100,
   ] as ProductsType;
