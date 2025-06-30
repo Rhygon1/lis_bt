96e4fc3b3f46305754b2abefa2d5a3677f69f1ce
rhygon <rhygonfn@gmail.com>
2025-06-29 20:22:18 -0400

    changed sliding size

--- app/components/collections.tsx
+++ app/components/collections.tsx
@@ -10,15 +10,15 @@ export default function Collections(props: propsType) {
     <div className="flex flex-col items-center">
       <p className="mt-10 text-xl font-[Overpass]">Shop by Collections</p>
       <div className="overflow-x-auto flex relative max-w-[95%] no-scrollbar m-4">
-        <Link className="basis-1/2 lg:basis-1/4 shrink-0" href="/search">
-          <div className="mx-1 basis-1/2 shrink-0 aspect-square overflow-hidden bg-black">
+        <Link className="basis-[37.5%] lg:basis-1/4 shrink-0" href="/search">
+          <div className="mx-1 basis-[37.5%] shrink-0 aspect-square overflow-hidden bg-black">
             <p className="h-full w-full flex items-center justify-center text-white font-[Overpass]">SHOP ALL</p>
           </div>
         </Link>
         {props.collections.map((a) => {
           return (
-            <Link key={`${a}`} className="basis-1/2 lg:basis-1/4 shrink-0" href={`/search?col=${a}`}>
-              <div className="mx-1 basis-1/2 shrink-0 aspect-square overflow-hidden">
+            <Link key={`${a}`} className="basis-[37.5%] lg:basis-1/4 shrink-0" href={`/search?col=${a}`}>
+              <div className="mx-1 basis-[37.5%] shrink-0 aspect-square overflow-hidden">
                 <img
                   src={`/categories/${a.toLowerCase()}.jpg`}
                   className="w-full h-full object-top object-cover"
