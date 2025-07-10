ea5494f859b52a90c9f4721959c9e288b3d97ed6
Arch <rhygonfn@gmail.com>
2025-07-10 19:40:47 -0400

    Removed image optimization

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "6jax627y1avwmget.public.blob.vercel-storage.com",
-      "uploadthing.com",
-      "dsd05nazui.ufs.sh"
-    ],
-    formats: ["image/webp"],
-    minimumCacheTTL: 60,
+    domains: [],
+		unoptimized: true
   },
   experimental: {
 	reactCompiler: true,
