0c6c6064f484dd5820e5278db59010306d226325
brungmail <bruh@gmail.com>
2025-07-13 09:54:16 -0400

    removed domain from nextjs config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [],
-		unoptimized: true
+    unoptimized: true,
   },
   experimental: {
 	reactCompiler: true,
